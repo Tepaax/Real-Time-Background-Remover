af167f3fcf00dea57e739237a4d22f730af62037
Tero Koponen <terokoponen@kamk.fi>
2025-02-13 22:17:43 +0200

    Modify app.py
    - Make window visible in OBS as capture device
    - replace image with green for croma keying
    - add my own code style changes

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
 import cv2
+import pyvirtualcam
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
 
 # connecting the internal camera (first camera index will be 0, it is the default)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1) # 1 seems to work with mobile
 
 # extracting the camera capture size
-width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH )), int( cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH ))
+height = int( cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
 
 # loading and resizing the background image
-background_image = cv2.resize(cv2.imread("bg_image.jpeg"), (width, height)) 
+#background_image = cv2.resize(cv2.imread("bg_image.jpeg"), (width, height)) 
 
 # creating segmentation instance for taking the foreground (the person).
 segmentor = SelfiSegmentation()
 
-# iterating the camera captures
-while True:
-    # Reading the captured images from the camera
-    ret, frame = cap.read()
+# Added a this for it to show up on OBS as a Video Capture device
+with pyvirtualcam.Camera(width, height, fps=60, fmt=pyvirtualcam.PixelFormat.BGR) as cam:
 
-    # segmenting the image
-    segmentated_img = segmentor.removeBG(frame, background_image, threshold=0.9)
+    while True:
+        # Reading the captured images from the camera
+        ret, frame = cap.read()
 
-    # concatenating the images horizontally
-    concatenated_img = cv2.hconcat([frame, segmentated_img])
+        # segmenting the image
+        green_bg = (0, 255, 0)
+        cutThreshold = 0.4 # higher = more cut, lower = less cut
+        segmentated_img = segmentor.removeBG(frame, green_bg, cutThreshold)
 
-    #cv2.imshow("Camera Capture", concatenated_img)
-    cv2.imshow("Camera Live", concatenated_img)
+        cv2.imshow("Camera", segmentated_img)
 
-    # ending condition
-    if cv2.waitKey(1) == ord('q'):
-        break
+        cam.send(segmentated_img)
+        cam.sleep_until_next_frame()
+
+        # ending condition
+        if cv2.waitKey(1) == ord('q'):
+            break
 
 # relasing the sources
 cap.release()
